8c9ea556402102132e3f7856a244d113748b40cd
padillasam <>
2021-07-22 11:14:02 -0500

    segundo commit desde fedora

--- src/colaborativogit/ColaborativoGit.java
+++ src/colaborativogit/ColaborativoGit.java
@@ -17,6 +17,7 @@ public class ColaborativoGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Git");
+        System.out.println("imprimo en fedora");
     }
     
 }
